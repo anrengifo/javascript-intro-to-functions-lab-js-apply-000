743f1fdfdf093960c63d2c879955f912590b98c5
anrengifo <rengifoad@gmail.com>
2018-02-17 04:53:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ function logWhisper (string) {
 }
 
 function sayHiToGrandma (string) {
-  if (string.lowercase===lowercase)  {
+  if (string===string.toLowerCase)  {
     return 'I can\'t hear you!'
   }
-  if (string.uppercase===uppercase) {
+  if (string===string.toUpperCase) {
     return "YES INDEED!"
   }
 }
