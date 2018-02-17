2d74387992ac899778411b55ad311bd61af62f98
anrengifo <rengifoad@gmail.com>
2018-02-17 04:51:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 function shout(string) {
   return string.toUpperCase()
 }
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logshout (string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper (string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma (string) {
+  if string.lowercase===lowercase === true {
+    return 'I can\'t hear you!'
+  }
+  if string.uppercase===uppercase {
+    return "YES INDEED!"
+  }
+}
